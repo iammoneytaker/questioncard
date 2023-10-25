205aaae79b8a8ffed181b7aaaac9817ddf2b782f
iammoneytaker <dnjs2618@naver.com>
2023-10-25 21:21:21 +0900

    ADD

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,5 +1,7 @@
 import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:in_app_review/in_app_review.dart';
 import 'package:package_info/package_info.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -17,6 +19,7 @@ class SettingScreen extends StatefulWidget {
 
 class _SettingScreenState extends State<SettingScreen> {
   bool _isGameSettingOn = true; // 게임 설정 ON/OFF 상태를 저장하는 변수
+  final InAppReview inAppReview = InAppReview.instance;
 
   @override
   void initState() {
@@ -108,8 +111,51 @@ class _SettingScreenState extends State<SettingScreen> {
     if (await canLaunchUrl(emailLaunchUri)) {
       await launchUrl(emailLaunchUri);
     } else {
+      _showEmailErrorDialog();
       print('Could not launch $emailLaunchUri');
     }
+  }
+
+  void _showEmailErrorDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('알림'),
+          content: const Text(
+            '메일앱을 불러오는데에 실패했습니다.\n잠시후에 시도해주세요.\n아니면 아래 이메일로 연락부탁드립니다.\nsangwon2618@gmail.com',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.grey,
+              height: 1.5,
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                '이메일 복사',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.lightBlue),
+              ),
+              onPressed: () {
+                Clipboard.setData(
+                        const ClipboardData(text: 'sangwon2618@gmail.com'))
+                    .then((result) {
+                  Navigator.of(context).pop(); // 대화상자 닫기
+                  showCustomSnackBar(context, "이메일이 복사되었습니다.", isSuccess: true);
+                });
+              },
+            ),
+            TextButton(
+              child: const Text('닫기'),
+              onPressed: () {
+                Navigator.of(context).pop(); // 대화상자 닫기
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   void _clickOepnKakao() {
@@ -118,34 +164,37 @@ class _SettingScreenState extends State<SettingScreen> {
     );
   }
 
+  void _requestReview() async {
+    if (await inAppReview.isAvailable()) {
+      inAppReview.requestReview();
+    } else {
+      showCustomSnackBar(
+        context,
+        '불러오기에 실패했습니다. 잠시뒤에 다시 시도해주세요',
+        isSuccess: false,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            '앱 기능',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
         ListTile(
-          leading: const Icon(Icons.refresh),
           title: const Text('현재까지 확인한 질문 초기화'),
           onTap: _showResetConfirmationDialog,
+          trailing: const Icon(Icons.refresh),
         ),
-        ListTile(
-          leading: const Icon(Icons.mail),
-          title: const Text('앱 문의하기'),
-          onTap: _sendEmail,
-        ),
-        ListTile(
-          leading: const Icon(Icons.star),
-          title: const Text('앱 리뷰'),
-          onTap: () {
-            // TODO: 앱 리뷰 로직 구현
-          },
-        ),
-        ListTile(
-          leading: const Icon(Icons.launch),
-          title: const Text('오픈카톡 참여'),
-          onTap: _clickOepnKakao,
-        ),
+        const Divider(), // 구분선 추가
         SwitchListTile(
-          title: const Text('질문카드에 게임 나오게하기'),
+          title: const Text('질문카드에 게임카드 나오게하기'),
           value: _isGameSettingOn,
           onChanged: (bool value) {
             setState(() {
@@ -154,6 +203,32 @@ class _SettingScreenState extends State<SettingScreen> {
             _saveGameSetting(value); // 게임 설정 저장
           },
         ),
+        const Divider(), // 구분선 추가
+        const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            '앱 정보',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
+        ListTile(
+          title: const Text('앱 문의하기'),
+          onTap: _sendEmail,
+          trailing: const Icon(Icons.mail),
+        ),
+        const Divider(), // 구분선 추가
+        ListTile(
+          title: const Text('앱 리뷰'),
+          onTap: _requestReview,
+          trailing: const Icon(Icons.star),
+        ),
+        const Divider(), // 구분선 추가
+        ListTile(
+          title: const Text('오픈카톡 참여'),
+          onTap: _clickOepnKakao,
+          trailing: const Icon(Icons.launch),
+        ),
+        const Divider(), // 구분선 추가
       ],
     );
   }
